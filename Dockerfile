89cc3ce27e9b74add14ed7fa62a10365daf914dc
igoreshirokov <igoreshirokov@yandex.ru>
2025-03-07 00:06:21 +0300

    Обновление Readme

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,6 @@ FROM python:3.13-slim
 
 WORKDIR /app
 
-# RUN groupadd -g 1000 appuser && \
-#     useradd -u 1000 -g appuser && \
-#     chown -R appuser:appuser /app
-
 USER appuser
 
 ENTRYPOINT ["/app/entrypoint.sh"]
--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,4 +2,24 @@
 
 # Устанавливаем зависимости
 pip install -r requirements.txt
-tail -f /dev/null
+# tail -f /dev/null
+
+python manage.py makemigrations
+echo "Созданы миграции"
+
+python manage.py migrate
+echo "Миграции применены"
+
+python manage.py createdemodata
+echo "Установлены демо данные"
+
+
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=admin@example.com
+export DJANGO_SUPERUSER_PASSWORD=admin123
+
+python manage.py createsuperuser --noinput
+echo "Суперпользователь создан admin & admin123"
+
+python manage.py runserver 0.0.0.0:8000
+
