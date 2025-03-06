16093227398ef4a88d6edf3701e55c7f52d29e00
igoreshirokov <igoreshirokov@yandex.ru>
2025-03-06 22:21:30 +0300

    Добавлен вход

--- app/app/urls.py
+++ app/app/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', products_views.index),
+    path('logout/', products_views.logout),
 ]
--- app/products/views.py
+++ app/products/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate as django_authenticate, get_user_model, login as django_login, logout as django_logout
 
 def index(request):
+    if request.user.is_authenticated:
+        return table(request)
+    else:
+        return login(request)
+
+def table(request):
     products = Product.objects.all()
     headers = {'id': 'ID', 
                'name': 'Название', 
@@ -16,3 +23,35 @@ def index(request):
     }
 
     return render(request,'index.html', context)
+
+def registration(request):
+    return render(request, 'registration.html')
+
+def login(request):
+    message = ''
+    if request.method == 'POST':
+        try:
+            user = authenticate(request)
+            if user is not None:
+                django_login(request, user)
+                return table(request)        
+        except Exception as e:
+            message = e
+
+    return render(request, 'login.html', {
+        'message': message
+    })
+
+def logout(request):
+    django_logout(request)
+    return redirect('/')
+
+def authenticate(request):
+    try:
+        user = get_user_model().objects.get(email=request.POST.get('email'))
+        user = django_authenticate(username=user.username, password=request.POST.get('password'))
+        return user
+    except get_user_model().DoesNotExist:
+        raise Exception('Email не зарегистрирован')
+    except:
+        raise Exception('Проверьте email или пароль')
